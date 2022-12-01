8e60b799681a1997e977efb1f4ce0a975c6b92f4
sawantanushil <113376359+sawantanushil@users.noreply.github.com>
2022-12-01 23:46:52 +0530

    union .ts file create
    union .ts file create

---
+++ union.js
@@ -0,0 +1,4 @@
+var sn;
+sn = "Codemind";
+sn = 10; //union is support for JS combine a diff datatypes use pipe oprator.
+console.log(sn);
---
+++ union.ts
@@ -0,0 +1,6 @@
+var sn:string | number;
+sn=`Codemind`;
+sn=10;  //union is support for JS combine a diff datatypes use pipe oprator.
+console.log(sn);
+
+               
