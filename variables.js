59ae076649a02efb558aa0d15f12e77791a0e8f2
sawantanushil <113376359+sawantanushil@users.noreply.github.com>
2022-12-01 20:03:22 +0530

    variables new changes
    variables new changes

--- variables.js
+++ variables.js
@@ -7,6 +7,20 @@ console.log(p);
 //homodenous array
 var aar1 = ["Angular", "JavaScript", "Recact"];
 //hetrogenous array
-var aar2 = ["Anushil", 123456, true];
+var aar2 = ['Anushil', 123456, true];
 aar2.push(".Netcore");
 console.log(aar2);
+var n1 = 10;
+console.log(n1);
+var s1 = "you are a crater of your destiny";
+var s2 = "Powerful people comes from Powerful Places";
+console.log(s1);
+console.log(s2);
+var b1 = true;
+var b2 = false;
+var b3;
+function test() {
+    b3 = false;
+    console.log(b3);
+}
+console.log(test());
--- variables.ts
+++ variables.ts
@@ -12,4 +12,23 @@ var aar2:Array<any>=['Anushil',123456,true];
 aar2.push(".Netcore");
 console.log(aar2);
 
+var n1:number=10;
+console.log(n1);
+
+var s1:string="you are a crater of your destiny";
+var s2:string="Powerful people comes from Powerful Places";
+console.log(s1);
+console.log(s2);
+
+var b1:boolean=true;
+var b2:boolean=false;
+var b3:boolean;
+
+function test(){
+   return b3=false;
+    console.log(b3); // unrechable code
+    
+}
+console.log(test());
+
 
